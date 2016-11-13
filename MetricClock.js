ddb6f71f0b484d40595489e5bfdc569f23fdc378
patrickmedaugh <patrickmedaugh@gmail.com>
2016-11-13 08:43:29 -0700

    Uses a metricTime object

--- MetricClock.js
+++ MetricClock.js
@@ -1,23 +1,19 @@
 var oneMetricSecond = .864;
-var d = new Date();
+var d = new Date(); // for now
 var hoursSeconds = d.getHours() * 60 * 60
 var minutesSeconds = d.getMinutes() * 60
-var seconds = d.getSeconds();
+var seconds = d.getSeconds(); // => 51
 
 var currentSeconds = hoursSeconds + minutesSeconds + seconds;
 
 var currentMetricSeconds = Math.round(currentSeconds / oneMetricSecond);
 
-var firstPlaceholder = currentMetricSeconds.toString().length - 2;
+var metricTime = {
+	seconds: currentMetricSeconds % 100,
+	minutes: Math.floor(currentMetricSeconds % 1000 / 10),
+	hours: currentMetricSeconds.toString()[0]
+}
 
-var secondPlaceholder = currentMetricSeconds.toString().length - 4;
-
-var currentMetricTimeArray = currentMetricSeconds.toString().split("")
-
-currentMetricTimeArray.splice(firstPlaceholder, 0, ":");
-currentMetricTimeArray.splice(secondPlaceholder, 0, ":");
-
-var currentMetricTime = currentMetricTimeArray.join("");
+var currentMetricTime = metricTime.hours + ":" + metricTime.minutes + ":" + metricTime.seconds;
 
 console.log(currentMetricTime)
-
